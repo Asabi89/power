8f90ab7ade2f83793867df4f9729cccf9713cc4b
IDOHOU David <idowuasabidavid@gmail.com>
2025-07-13 12:08:36 +0100

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -140,6 +140,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_URL = '/accounts/login/'
 
 # ============================================================================
 # REST FRAMEWORK CONFIGURATION
